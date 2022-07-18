8e4de9640eedb6777e5433def27225e8cd98c784
Paulo Henrique <paulo.silva@southsystem.com.br>
2022-07-18 15:12:23 -0300

    feat: changes on classname and enum

--- src/components/Sidebar/enum.tsx
+++ src/components/Sidebar/enum.tsx
@@ -17,219 +17,225 @@ import {
 } from "react-icons/md";
 import { SiYoutubemusic } from "react-icons/si";
 
-export const openSidebarItems = [
+export const openSidebarItems = [ 
   {
     label: "Início",
+    showClosed: true,
     icon: <TiHome size={24} />,
     breakList: false
   },
   {
     label: "Explorar",
+    showClosed: true,
     icon: <AiOutlineCompass size={24} />,
     breakList: false
   },
   {
     label: "Shorts",
+    showClosed: true,
     icon: <FiChevronsLeft size={24} />,
     breakList: false
   },
   {
     label: "Inscrições",
+    showClosed: true,
     icon: <MdOutlineSubscriptions size={24} />,
     breakList: true
   },
   {
     label: "Biblioteca",
+    showClosed: true,
     icon: <MdOutlineVideoLibrary size={24} />,
     breakList: false
   },
   {
     label: "Histórico",
+    showClosed: false,
     icon: <MdHistory size={24} />,
     breakList: false
   },
   {
     label: "Seus vídeos",
+    showClosed: false,
     icon: <MdOutlinePlayCircle size={24} />,
     breakList: false
   },
   {
     label: "Assistir mais tarde",
+    showClosed: false,
     icon: <MdAccessTime size={24} />,
     breakList: false
   },
   {
     label: "Vídeos marcados com gostei",
+    showClosed: false,
     icon: <AiOutlineLike size={24} />,
     breakList: true
   },
   {
     headerTitle: "INSCRIÇÕES",
     label: "Música",
+    showClosed: false,
     icon: <SiYoutubemusic size={24} />,
     breakList: false
   },
   {
     label: "Esportes",
+    showClosed: false,
     icon: <MdSportsBaseball size={24} />,
     breakList: false
   },
   {
     label: "Jogos",
+    showClosed: false,
     icon: <MdGames size={24} />,
     breakList: false
   },
   {
     label: "Filmes",
+    showClosed: false,
     icon: <MdLocalMovies size={24} />,
     breakList: true,
   },
   {
     headerTitle: "MAIS DO YOUTUBE",
     label: "Youtube Premium",
+    showClosed: false,
     icon: <AiOutlineYoutube size={24} />,
     breakList: false
   },
   {
     label: "Filmes",
+    showClosed: false,
     icon: <MdOutlineLocalMovies size={24} />,
     breakList: false
   },
   {
     label: "Jogos",
+    showClosed: false,
     icon: <MdOutlineGames size={24} />,
     breakList: false
   },
   {
     label: "Ao vivo",
+    showClosed: false,
     icon: <MdOutlineOnlinePrediction size={24} />,
     breakList: false
   },
   {
     label: "Aprender",
+    showClosed: false,
     icon: <BsLightbulb size={24} />,
     breakList: false
   },
   {
     label: "Início",
+    showClosed: false,
     icon: <TiHome size={24} />,
     breakList: false
   },
   {
     label: "Explorar",
+    showClosed: false,
     icon: <AiOutlineCompass size={24} />,
     breakList: false
   },
   {
     label: "Shorts",
+    showClosed: false,
     icon: <FiChevronsLeft size={24} />,
     breakList: false
   },
   {
     label: "Inscrições",
+    showClosed: false,
     icon: <MdOutlineSubscriptions size={24} />,
     breakList: true
   },
   {
     label: "Biblioteca",
+    showClosed: false,
     icon: <MdOutlineVideoLibrary size={24} />,
     breakList: false
   },
   {
     label: "Histórico",
+    showClosed: false,
     icon: <MdHistory size={24} />,
     breakList: false
   },
   {
     label: "Seus vídeos",
+    showClosed: false,
     icon: <MdOutlinePlayCircle size={24} />,
     breakList: false
   },
   {
     label: "Assistir mais tarde",
+    showClosed: false,
     icon: <MdAccessTime size={24} />,
     breakList: false
   },
   {
     label: "Vídeos marcados com gostei",
+    showClosed: false,
     icon: <AiOutlineLike size={24} />,
     breakList: true
   },
   {
     headerTitle: "INSCRIÇÕES",
     label: "Música",
+    showClosed: false,
     icon: <SiYoutubemusic size={24} />,
     breakList: false
   },
   {
     label: "Esportes",
+    showClosed: false,
     icon: <MdSportsBaseball size={24} />,
     breakList: false
   },
   {
     label: "Jogos",
+    showClosed: false,
     icon: <MdGames size={24} />,
     breakList: false
   },
   {
     label: "Filmes",
+    showClosed: false,
     icon: <MdLocalMovies size={24} />,
     breakList: true,
   },
   {
     headerTitle: "MAIS DO YOUTUBE",
     label: "Youtube Premium",
+    showClosed: false,
     icon: <AiOutlineYoutube size={24} />,
     breakList: false
   },
   {
     label: "Filmes",
+    showClosed: false,
     icon: <MdOutlineLocalMovies size={24} />,
     breakList: false
   },
   {
     label: "Jogos",
+    showClosed: false,
     icon: <MdOutlineGames size={24} />,
     breakList: false
   },
   {
     label: "Ao vivo",
+    showClosed: false,
     icon: <MdOutlineOnlinePrediction size={24} />,
     breakList: false
   },
   {
     label: "Aprender",
+    showClosed: false,
     icon: <BsLightbulb size={24} />,
     breakList: false
-  },
-
+  }, 
 ];
-
-export const closedSidebarItems = [
-  {
-    id: 1,
-    label: "Início",
-    icon: <TiHome size={24} />
-  },
-  {
-    id: 2,
-    label: "Explorar",
-    icon: <AiOutlineCompass size={24} />
-  },
-  {
-    id: 3,
-    label: "Shorts",
-    icon: <FiChevronsLeft size={24} />
-  },
-  {
-    id: 4,
-    label: "Inscrições",
-    icon: <MdOutlineSubscriptions size={24} />
-  },
-  {
-    id: 5,
-    label: "Biblioteca",
-    icon: <MdOutlineVideoLibrary size={24} />
-  },
-
-]
--- src/components/Sidebar/sidebar.tsx
+++ src/components/Sidebar/sidebar.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
-import { closedSidebarItems, openSidebarItems } from "./enum";
+import { openSidebarItems } from "./enum";
 
 export const Sidebar = () => {
   const [openSidebar, setOpenSidebar] = useState(false);
@@ -10,38 +10,20 @@ export const Sidebar = () => {
   };
 
   return (
-    <>
-      {openSidebar ? (
-        <aside className="bg-white fixed top w-[72px] h-screen flex flex-col items-center">
-          <button className="mt-4" onClick={handleOpenSidebar}><AiOutlineMenu size={24} /></button>
-          <div className="w-full mt-6">
-            {closedSidebarItems.map(item =>
-              <div key={item.label} className="px-6">
-                <button className="flex items-center flex-col justify-center w-full py-4 hover:bg-gray-200 ">
-                  {item.icon}
-                  <span className="text-xs">{item.label}</span>
-                </button>
-              </div>
-            )}
+    <aside className={`bg-white fixed h-screen flex flex-col ${openSidebar && "top w-[72px] items-center"}`}>
+      <button className={`${openSidebar ? "mt-4" : "mt-4 ml-6"}`} onClick={handleOpenSidebar}><AiOutlineMenu size={24} /></button>
+      <div className={` mt-6 ${openSidebar ? "w-full" : "w-60 overflow-y-auto scrollbar"}`}>
+        {openSidebarItems.map(item =>
+          <div key={item.label} className="px-6 hover:bg-gray-200">
+            {item.headerTitle && !openSidebar && <p className="py-2 text-gray-500 font-medium">{item.headerTitle}</p>}
+            <button className={`flex items-center w-full ${openSidebar ? "flex-col justify-center py-4" : "gap-6 h-10"} ${openSidebar && !item.showClosed && "hidden"}`}>
+              {item.icon}
+              <span className={openSidebar ? "text-xs" : "text-lg truncate"}>{item.label}</span>
+            </button>
+            {item.breakList && !openSidebar && <div className="border-t my-3" />}
           </div>
-        </aside>
-      ) : (
-        <aside className="bg-white fixed h-screen flex flex-col ">
-          <button className="mt-4 ml-6" onClick={handleOpenSidebar}><AiOutlineMenu size={24} /></button>
-          <div className="w-60 mt-6 overflow-y-auto scrollbar">
-            {openSidebarItems.map(item =>
-              <div key={item.label} className="px-6">
-                {item.headerTitle && <p className="py-2 text-gray-500 font-medium">{item.headerTitle}</p>}
-                <button className="flex items-center gap-6 h-10 w-full hover:bg-gray-200" >
-                  {item.icon}
-                  <span className="text-lg truncate">{item.label}</span>
-                </button>
-                {item.breakList && <div className="border-t my-3" />}
-              </div>
-            )}
-          </div>
-        </aside>
-      )}
-    </>
+        )}
+      </div>
+    </aside>
   )
 }
